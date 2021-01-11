95fcea206d69e39d85beeff551dea22184791890
Alexander Khasanov <alexander.kkkk@yandex.ru>
2021-01-11 22:56:56 +0400

    кастомизировал ресайклер немножко

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.myapplication;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -10,6 +17,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView rv;
+    EditText etSearch;
+    GradientDrawable etBackground = new GradientDrawable();
+    View divider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +32,46 @@ public class MainActivity extends AppCompatActivity {
 
     private void bind() {
         rv = findViewById(R.id.rv);
+        etSearch = findViewById(R.id.etSearch);
+        divider = findViewById(R.id.divider);
+
+        etBackground.setCornerRadii(new float[]{60f, 60f, 60f, 60f, 60f, 60f, 60f, 60f});
+        etBackground.setColor(getResources().getColor(R.color.blue));
+        etSearch.addTextChangedListener(getTextWatcher());
+        etSearch.setBackground(etBackground);
     }
+
 
     private void initRecycler() {
         ListAdapter adapter = new ListAdapter();
-        adapter.setData(getNumberList(550));
+        adapter.setData(getNumberList(25));
         rv.setAdapter(adapter);
+    }
+
+    private TextWatcher getTextWatcher() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 2) {
+                    etBackground.setCornerRadii(new float[]{60f, 60f, 60f, 60f, 0f, 0f, 0f, 0f});
+                    divider.setVisibility(View.VISIBLE);
+                    rv.setVisibility(View.VISIBLE);
+                } else {
+                    etBackground.setCornerRadii(new float[]{60f, 60f, 60f, 60f, 60f, 60f, 60f, 60f});
+                    divider.setVisibility(View.GONE);
+                    rv.setVisibility(View.GONE);
+                }
+                etSearch.setBackground(etBackground);
+            }
+        };
     }
 
     private List<Integer> getNumberList(int count) {
